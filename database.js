48a260882b60523e30a4b51f32e1dfb5256d74e3
LuoFederico <luo.hang.li.federico@iisgalvanimi.edu.it>
2021-05-10 10:30:58 +0000

    bo

---
+++ database.js
@@ -0,0 +1,56 @@
+/*
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+*/
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'mypassword',
+        server: 'localhost', 
+        database: 'SchoolDB' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from Student', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
